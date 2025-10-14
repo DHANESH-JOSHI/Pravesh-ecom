e121c84b8a38305bed63b31256d869d97e4512b8
Md Asharaf <718romanempire@gmail.com>
2025-10-14 11:49:01 +0530

    add seeder functionality to initialize admin and regular users

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import "./app/config/moduleAlias"
 import mongoose from 'mongoose';
 import app from './app';
 import config from "./app/config"
+import { seedDatabase } from "./seeder";
 async function main() {
     try {
         await mongoose.connect(config.DATABASE_URL as string);
-
+        console.log('[DB] Database connected successfully');
+        await seedDatabase();
         app.listen(config.PORT, () => {
             console.log(`server is running on port ${config.PORT}`)
         })
@@ -14,5 +16,4 @@ async function main() {
         console.log(err)
     }
 }
-
 main();
